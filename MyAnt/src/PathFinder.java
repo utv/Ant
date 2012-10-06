72749b7ac6919365238a68fb006f8c48d78a1e71
Amata <k9amata@gmail.com>
2012-10-06 04:47:05 -0700

    add issuePreOrder()

--- MyAnt/src/PathFinder.java
+++ MyAnt/src/PathFinder.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -22,7 +23,7 @@ public class PathFinder {
 		List<Aim> directions = gameManager.getDirections(ant, target);
 		for(Aim direction : directions){
 			if(gameManager.getIlk(ant, direction).isPassable()){
-				gameManager.issueOrder(ant, direction);
+				gameManager.issuePreOrder(ant, direction);
 				break;
 			}
 		}
@@ -31,32 +32,72 @@ public class PathFinder {
 	public Tile getAnt4Target(Tile target) {
 		Tile tile = null;
 		int searchCount = 0;
-		int searchLimit = 500;
+		int searchDepth = 500;
 		/*
 		 * BFS here!
 		 */
 		Queue<Tile> qe = new LinkedList<Tile>();
+		HashSet<Tile> visitedTile = new HashSet<Tile>();
 		qe.add(target);
 		
-		while(!qe.isEmpty()&& searchCount < searchLimit ){		
+		while(!qe.isEmpty()&& searchCount < searchDepth ){		
 			tile = qe.remove();
-			if(isMyAnt(tile)){	//found the best ant for this food
+			if(isMyAnt(tile)){	//found best ant for this food
+				//DEBUG
+				System.out.println("number of search = " + searchCount);
 				return tile;
 			}
 			for(Aim direction : Aim.values()){
-				Tile neighbor;
-				if(gameManager.getIlk(tile, direction).isPassable()){
-					neighbor = gameManager.getTile(tile, direction);
-					qe.add(neighbor);
+				if(isPassAbleTile(tile, direction)){
+					Tile neighbor = gameManager.getTile(tile, direction);
+					if(!visitedTile.contains(neighbor)){
+						qe.add(neighbor);
+						visitedTile.add(neighbor);
+					}
 				}
-				
 			}
 			searchCount++;
 		}
-		
+		//DEBUG
+		System.out.println("number of search = " + searchCount);
 		return null;	//can't find any ants for this food
 	}
 	
+	private boolean isPassAbleTile(Tile tile, Aim direction) {
+		return gameManager.getIlk(tile, direction).isPassable();
+	}
+
+	public Tile getAnt4TargetNoMap(Tile target) {
+		Tile tile = null;
+		int searchCount = 0;
+		int searchDepth = 500;
+		/*
+		 * BFS here!
+		 */
+		Queue<Tile> qe = new LinkedList<Tile>();
+		//HashSet<Tile> visitedTile = new HashSet<Tile>();
+		qe.add(target);
+		
+		while(!qe.isEmpty()&& searchCount < searchDepth ){		
+			tile = qe.remove();
+			if(isMyAnt(tile)){	//found the best ant for this food
+				//DEBUG
+				System.out.println("number of search = " + searchCount);
+				return tile;
+			}
+			for(Aim direction : Aim.values()){
+				if(isPassAbleTile(tile, direction)){
+					Tile neighbor = gameManager.getTile(tile, direction);
+					qe.add(neighbor);
+				}
+			}
+			searchCount++;
+		}
+		//DEBUG
+		System.out.println("number of search = " + searchCount);
+		return null;	//can't find any ants for this food
+	}
+
 	private boolean isMyAnt(Tile tile) {
 		return gameManager.map[tile.getRow()][tile.getCol()].equals(Ilk.MY_ANT);
 	}
